09079b3b58df478e5915b8c80e86232b0da078e7
kashew-developers <kashewdevelopers@gmail.com>
2019-11-28 00:17:21 +0530

    Create ColorShift ScreenSaver

---
+++ app/src/main/java/in/kashewdevelopers/screensavers/MainActivity.java
@@ -0,0 +1,18 @@
+package in.kashewdevelopers.screensavers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, ColorShifter.class));
+    }
+}
