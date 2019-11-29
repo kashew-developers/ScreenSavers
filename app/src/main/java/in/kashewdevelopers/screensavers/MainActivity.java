89e00f788343659e429c659ab8ddac7e58d5d37c
kashew-developers <kashewdevelopers@gmail.com>
2019-11-29 13:01:38 +0530

    Update MainActivity
    Show ColorShifterFragment and RandomLinesFragment option

--- app/src/main/java/in/kashewdevelopers/screensavers/MainActivity.java
+++ app/src/main/java/in/kashewdevelopers/screensavers/MainActivity.java
@@ -1,16 +1,67 @@
 package in.kashewdevelopers.screensavers;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import androidx.fragment.app.FragmentActivity;
+
+import in.kashewdevelopers.screensavers.Fragments.ColorShifterFragment;
+
+public class MainActivity extends FragmentActivity {
+
+    boolean backPressed;
+    Toast onBackPressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        backPressed = false;
+        onBackPressed = Toast.makeText(this, "Press Back again to exit", Toast.LENGTH_SHORT);
+
+        final Intent i = new Intent(this, FragmentHolderActivity.class);
+
+        findViewById(R.id.colorShifterBox).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i.putExtra("ScreenSaverId", 0);
+                startActivity(i);
+            }
+        });
+
+        findViewById(R.id.randomLinesBox).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i.putExtra("ScreenSaverId", 1);
+                startActivity(i);
+            }
+        });
+
     }
+
+    @Override
+    public void onBackPressed() {
+        if( backPressed ) super.onBackPressed();
+
+        backPressed = true;
+        onBackPressed.show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                backPressed = false;
+            }
+        }, 1500);
+    }
+
+    @Override
+    protected void onDestroy() {
+        onBackPressed.cancel();
+        super.onDestroy();
+    }
+
 }
